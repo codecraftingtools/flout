479032d145ac54e13962f554c1f880e7fa22d3ea
Jeff Webb <jeff.webb@codecraftsmen.org>
2020-11-06 23:05:10 -0600

    Changed branch name from master to main in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -181,4 +181,4 @@ epub_exclude_files = ['search.html']
 intersphinx_mapping = {'cc': ('https://www.codecraftsmen.org', None)}
 
 extlinks = {
-    'repo': ('https://github.com/codecraftingtools/flout/blob/master/%s','')}
+    'repo': ('https://github.com/codecraftingtools/flout/blob/main/%s','')}
